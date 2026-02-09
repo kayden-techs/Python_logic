cb742b3666e73eb3c0d4dde717a5956ee8da6b41
kayden-techs <ekechukwuemekagreat@gmail.com>
2026-02-09 11:28:12 +0100

    Day 1: variables, input, output

---
+++ day1_variables.py
@@ -0,0 +1,21 @@
+# Day 1: Input and Output
+
+name = input("Enter your name: ")
+age =int(input("Enter your age: "))
+next_year_age = age + 1
+print("Hello", name)
+print("You are", age, "years old")
+print("Next year i will be" , next_year_age)
+
+# Day 1: Variables and Output
+
+name = "Kayden"
+age = 21
+height = 5.7
+is_student = True
+
+
+print(name)
+print(age)
+print(height)
+print(is_student)
